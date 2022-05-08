95045962486c33eec2a7eeadec8b9d0888b84623
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-05-08 11:27:19 +0900

    feat: add `/users` endpoint

--- litestream-apprunner/prisma/seed.ts
+++ litestream-apprunner/prisma/seed.ts
@@ -1,3 +1,13 @@
-async function seed() {}
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function seed() {
+  for (let i = 0; i < 10; i++) {
+    await prisma.user.create({
+      data: { name: `user${i}`, email: `user${i}@example.com` },
+    });
+  }
+}
 
 seed();
