5b24fd57716a58b0efdef69db284fee518eabebb
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-05-14 11:52:15 +0900

    feat(copilot-golang): init

---
+++ copilot-golang/graph/schema.resolvers.go
@@ -0,0 +1,30 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"copilot-golang/graph/generated"
+	"copilot-golang/graph/model"
+	"fmt"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	return []*model.Todo{
+		{ID: "1", Text: "todo1", Done: false, User: &model.User{ID: "1", Name: "user1"}},
+	}, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
